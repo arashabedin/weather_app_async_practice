1eb7ca02fc8d6613f8d72620c212890881be097a
arashabedin <arash.abedin@gmail.com>
2018-06-10 23:59:36 +0200

    more comments

--- playground/async- basics.js
+++ playground/async- basics.js
@@ -4,7 +4,10 @@ setTimeout(() => {
     console.log('Inside of callback');
 }, 2000);
 
-//This would be called after next log eventhough it's zero timeout
+// This would be called after next log eventhough it's zero timeout.
+// The reason is the process of event loop, The logs that are not
+// surronded by setTimeout would be executed at Call Stack first and
+// but the rest are processed into Node API and Callback Queue first
 setTimeout(()=>{
     console.log("Second settimeout");
 },0);
