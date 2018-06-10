265c5cbbad287e787d0ee1534f7529866aa4318d
arashabedin <arash.abedin@gmail.com>
2018-06-10 23:46:15 +0200

    add comment

--- playground/async- basics.js
+++ playground/async- basics.js
@@ -4,6 +4,7 @@ setTimeout(() => {
     console.log('Inside of callback');
 }, 2000);
 
+//This would be called after next log eventhough it's zero timeout
 setTimeout(()=>{
     console.log("Second settimeout");
 },0);
