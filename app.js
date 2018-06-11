151d2e8880b68b823162def0ab49c4e3d61dc29c
arashabedin <arash.abedin@gmail.com>
2018-06-12 00:27:27 +0200

    more stuff on google map api

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const request = require('request');
 
 request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=%22ermelundsvej%2043',
+    url:'https://maps.googleapis.com/maps/api/geocode/json?address=%22ermelundsvej',
     json:true
 },(error, response, body)=>{
 
-console.log(JSON.stringify(body, undefined, 2));
+//console.log(JSON.stringify(body, undefined, 2));
+console.log(`Address: ${body.results[0].formatted_address}`);
+console.log(`Lattitude: ${body.results[0].geometry.location.lat}`)
+console.log(`Lanitude: ${body.results[0].geometry.location.lng}`)
 });
