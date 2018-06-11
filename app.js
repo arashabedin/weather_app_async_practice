f0321a77605296419dac4de4f63bc772ce806cb7
arashabedin <arash.abedin@gmail.com>
2018-06-12 00:58:45 +0200

    add some error handling

--- app.js
+++ app.js
@@ -22,8 +22,15 @@ request({
    json:true
 },(error, response, body)=>{
 
-//console.log(JSON.stringify(body, undefined, 2));
-console.log(`Address: ${body.results[0].formatted_address}`);
-console.log(`Lattitude: ${body.results[0].geometry.location.lat}`)
-console.log(`Lanitude: ${body.results[0].geometry.location.lng}`)
+    if(error){
+        console.log("Unable to connect to google servers.");
+    }else if(body.status === 'ZERO_RESULTS'){
+        console.log("Unable to find that address.");
+    }else if(body.status === 'OK'){
+
+        //console.log(JSON.stringify(body, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Lanitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
